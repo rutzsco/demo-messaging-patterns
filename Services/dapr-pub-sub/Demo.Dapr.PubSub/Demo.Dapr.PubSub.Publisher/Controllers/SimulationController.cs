b9991f68222a28ac6af82468e9811f050e08a320
Scott Rutz <scrutz@microsoft.com>
2022-04-26 14:06:42 -0500

    cp

---
+++ Services/dapr-pub-sub/Demo.Dapr.PubSub/Demo.Dapr.PubSub.Publisher/Controllers/SimulationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Demo.Dapr.PubSub.Publisher.Model;
+using Dapr.Client;
+
+namespace Demo.Dapr.PubSub.Publisher.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SimulationController : ControllerBase
+    {
+        private DaprClient _daprClient;
+        private IConfiguration _configuration;
+
+        public SimulationController(DaprClient daprClient, IConfiguration configuration)
+        {
+            _daprClient = daprClient;
+            _configuration = configuration;
+        }
+
+        [HttpGet("publish")]
+        public async Task<IActionResult> Publish()
+        {
+            await _daprClient.PublishEventAsync<Telemetry>("pubsub-servicebus", "telemetry", new Telemetry("ASSET001", "Tempurature", "32"));
+            
+            Console.WriteLine($"{DateTime.UtcNow} - Published event!");
+
+            return new OkObjectResult("OK");
+        }
+    }
+}
