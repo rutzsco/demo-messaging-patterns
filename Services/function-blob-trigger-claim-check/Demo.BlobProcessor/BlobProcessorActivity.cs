da2c1b64a04987078aa1f741e9fbe30560b21317
Scott Rutz <scrutz@microsoft.com>
2022-04-28 07:21:34 -0500

    Update BlobProcessorActivity.cs

--- Services/function-blob-trigger-claim-check/Demo.BlobProcessor/BlobProcessorActivity.cs
+++ Services/function-blob-trigger-claim-check/Demo.BlobProcessor/BlobProcessorActivity.cs
@@ -6,15 +6,18 @@ using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
 using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace Demo.BlobProcessor
 {
     public static class BlobProcessorActivity
     {
         [FunctionName("BlobProcessorActivity")]
-        public static void Run([BlobTrigger("messages/{name}", Source = BlobTriggerSource.EventGrid, Connection = "BlobStorageConnection")] Stream blobStream, string name, ILogger log)
+        public static async Task Run([EventGridTrigger] EventGridEvent eventGridEvent, [Blob("{data.url}", FileAccess.Read)] Stream blobStream, ILogger log)
         {
-            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {blobStream.Length} Bytes");
+            var createdEvent = ((JObject)eventGridEvent.Data).ToObject<StorageBlobCreatedEventData>();
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{createdEvent.Url} \n Size: {blobStream.Length} Bytes");
         }
     }
 }
