1a3900fea055e7672411fe5079b7386343927be9
Scott Rutz <scrutz@microsoft.com>
2022-05-23 15:03:42 -0500

    cp

---
+++ Services/dapr-pub-sub/Demo.Dapr.PubSub/Demo.Dapr.PubSub.Subscriber/Controllers/TelemetryController.cs
@@ -0,0 +1,35 @@
+﻿using Dapr;
+using Dapr.Client;
+using Demo.Dapr.PubSub.Subscriber.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Dapr.PubSub.Subscriber.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TelemetryController : ControllerBase
+    {
+        private DaprClient _daprClient;
+        private IConfiguration _configuration;
+
+        public TelemetryController(DaprClient daprClient, IConfiguration configuration)
+        {
+            _daprClient = daprClient;
+            _configuration = configuration;
+        }
+
+        [Topic("pubsub-sb", "telemetry")]
+        [HttpPost("processtelemetry")]
+        public async Task ProcessTelemetry([FromBody] TelemetryData telemetryData)
+        {
+            try
+            {
+                Console.WriteLine($"{DateTime.UtcNow} - Event received processrabbitmq - AssetId: {telemetryData.AssetId}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Services/dapr-pub-sub/Demo.Dapr.PubSub/Demo.Dapr.PubSub.Subscriber/Model/TelemetryData.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Demo.Dapr.PubSub.Subscriber.Model
+{
+    public class TelemetryData
+    {
+        public TelemetryData(string assetId, string metricName, string metricValue)
+        {
+            AssetId = assetId;
+            MetricName = metricName;
+            MetricValue = metricValue;
+        }
+
+        [JsonPropertyName("assetId")]
+        public string AssetId { get; set; }
+
+        [JsonPropertyName("metricName")]
+        public string MetricName { get; set; }
+
+        [JsonPropertyName("metricValue")]
+        public string MetricValue { get; set; }
+    }
+}
