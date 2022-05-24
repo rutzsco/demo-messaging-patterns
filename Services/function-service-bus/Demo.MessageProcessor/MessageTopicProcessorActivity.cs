e2cae3244ce2e1ad305a483df779b661c815cdba
Scott Rutz <scrutz@microsoft.com>
2022-05-24 14:12:53 -0500

    Create MessageTopicProcessorActivity.cs

---
+++ Services/function-service-bus/Demo.MessageProcessor/MessageTopicProcessorActivity.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace Demo.MessageProcessor
+{
+    public class MessageTopicProcessorActivity
+    {
+        [FunctionName("MessageTopicProcessorActivity")]
+        public void Run([ServiceBusTrigger("telemetry002", Connection = "ServiceBusConnectionString")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+        }
+    }
+}
